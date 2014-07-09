72ca13c65c44586e85b5d1f678ff582fd926d270
Mayra Cabrera <mcabrera1087@gmail.com>
2014-07-08 21:07:18 -0500

    Include a internationalization: - Set the default locale for the application and provide a means for the user to select an alternate locale - Creates translation files for text fields, currency amounts, errors and model names - Alters layouts and views to call out the i18n module by way of the t() helper in order to translate textual portions of the interface

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,20 @@ Depot::Application.routes.draw do
 
   controller :sessions do
     get 'login' => :new
-
     post 'login' => :create
-
     delete "logout" => :destroy
   end
   
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products do
+      get :who_bought, on: :member
+    end
 
-  get "store/index"
-
-  resources :products do
-    get :who_bought, on: :member
+    root to: 'store#index', as: 'store'
   end
-
-  root to: 'store#index', as: 'store'
+  
 end
